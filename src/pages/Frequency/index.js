8b997c59f20d78bd2555001bc7b12bbdb534479d
Gabriel Dias <gabriel.cardoso@totvs.com.br>
2020-04-17 20:22:24 -0300

    realizando chamada

--- src/pages/Frequency/index.js
+++ src/pages/Frequency/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, FlatList, Text, View, ScrollView } from 'react-native';
+import {
+  SafeAreaView, StatusBar, StyleSheet, FlatList, Text, View, ScrollView, Image, Alert, Modal, Picker
+} from 'react-native';
 import { format } from 'date-fns'
 
-import Icon from 'react-native-vector-icons/AntDesign';
+import { DataTable, Button } from 'react-native-paper';
 
-import { DataTable, Button, } from 'react-native-paper';
-
+import jesus from "../../assets/jesus_title.jpg";
 
 import api from "../../services/api";
 
@@ -13,10 +14,18 @@ export default class Frequency extends Component {
   state = {
     students: [],
     frequencies: [],
-    date: format(new Date(), 'dd/MM/yyyy')
+    date: format(new Date(), 'dd/MM/yyyy'),
+    modalVisible: false,
+    users: [],
+    teacher: '',
+    devotional: '',
   };
+
   async componentDidMount() {
+    const { data } = await api.get("/users");
     const students = await api.get("/student");
+    const frequencies = await api.get("/frequency");
+
     let arrayStudent = []
     students.data.map(student => {
       const obj = {
@@ -26,12 +35,12 @@ export default class Frequency extends Component {
       }
       arrayStudent.push(obj)
     })
-    const frequencies = await api.get("/frequency");
-    this.setState({ students: arrayStudent, frequencies })
+
+    this.setState({ users: data, students: arrayStudent, frequencies })
   }
 
   render() {
-    const { students, date } = this.state;
+    const { students, date, modalVisible, users } = this.state;
 
     const handleFrequency = (student, index) => {
       let newStudents = this.state.students;
@@ -43,12 +52,23 @@ export default class Frequency extends Component {
       this.setState({ students: newStudents })
     }
 
+    const handleAccomplish = async () => {
+      this.setState({ modalVisible: true })
+    }
+
     const handleSave = async () => {
       const frequency = this.state.students.map(student => {
         return { id_user: student.id, training_date: format(new Date(), 'yyyy-MM-dd'), frequency: student.frequency }
       })
-      const response = await api.post("/frequency", frequency);
-      alert(JSON.stringify(response.data))
+      const respFrequency = await api.post("/frequency", frequency);
+      const respTraining = await api.post("/training", { teacher: this.state.teacher.id, devotional: this.state.devotional.id })
+      this.setState({ modalVisible: false })
+
+      if (respFrequency.data.ok && respTraining.data.ok) {
+        alert('Chamada salva com sucesso')
+      } else {
+        alert('Falha ao salvar chamada')
+      }
     }
 
     return (
@@ -59,7 +79,66 @@ export default class Frequency extends Component {
           backgroundColor="#FFFF"
         />
 
-        <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ backgroundColor: '#fff', height: 60, paddingVertical: 5, alignItems: 'center', borderWidth: 1, borderTopColor: '#FFF', borderBottomColor: '#dcdcdc', borderLeftColor: '#FFF', borderRightColor: '#FFF' }}>
+          <Image
+            source={jesus}
+          />
+        </View>
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+
+            <View style={styles.modalView}>
+
+              <Text style={styles.label}>Quem foi o professor?</Text>
+              <View style={styles.textInput}>
+                <Picker
+                  selectedValue={this.state.teacher}
+                  style={{ height: 50, width: '100%' }}
+                  onValueChange={(itemValue) => this.setState({ teacher: itemValue })}
+                >
+                  {this.state.users.map((user, index) => {
+                    return (<Picker.Item label={user.name} value={user} key={user.id} />)
+                  })}
+                </Picker>
+              </View>
+
+
+              <Text style={styles.label}>Quem fez a Devocional?</Text>
+              <View style={styles.textInput}>
+                <Picker
+                  selectedValue={this.state.devotional}
+                  style={{ height: 50, width: '100%' }}
+                  onValueChange={(itemValue) => this.setState({ devotional: itemValue })}
+                >
+                  {this.state.users.map((user, index) => {
+                    return (<Picker.Item label={user.name} value={user} key={user.id} />)
+                  })}
+                </Picker>
+
+              </View>
+
+              <Button mode="contained" style={{ marginTop: 10, width: '80%' }} color="red" onPress={() => handleSave()}>
+                Salvar Chamada
+              </Button>
+
+              <Button mode="contained" style={{ marginTop: 10, width: '80%' }} color="black" onPress={() => this.setState({ modalVisible: false })}>
+                Cancelar
+              </Button>
+
+            </View>
+
+          </View>
+        </Modal>
+
+        <ScrollView showsVerticalScrollIndicator={false} style={{ marginBottom: 60 }}>
           <View style={styles.title}>
             <Text>Data da chamada: {date}</Text>
           </View>
@@ -88,9 +167,9 @@ export default class Frequency extends Component {
 
           </DataTable>
 
-          <Button mode="contained" style={{ margin: 10 }} color="red" onPress={() => handleSave()}>
+          <Button mode="contained" style={{ margin: 10, width: '100%' }} color="red" onPress={() => handleAccomplish()}>
             Realizar Chamada
-        </Button>
+          </Button>
         </ScrollView>
       </SafeAreaView >
     );
@@ -141,5 +220,33 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
     elevation: 4,
+  },
+  centeredView: {
+    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+    height: '100%',
+  },
+  modalView: {
+    padding: 20,
+    marginHorizontal: 20,
+    marginTop: '20%',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    paddingBottom: 60
+  },
+  modalText: {
+    padding: 10
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "gray",
+    fontSize: 16,
+    color: "#444",
+    height: 50,
+    width: '80%',
+    borderRadius: 2
+  },
+  label: {
+    padding: 10,
+    marginTop: 10
   }
 });
